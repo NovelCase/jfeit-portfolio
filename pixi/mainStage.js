9e2fb369c1cd8b32ae5e1e6bfe135ad980e72416
Jackie Feit <jackiefeit94@gmail.com>
2021-01-19 09:51:48 -0500

    chore: quilting effect

--- pixi/mainStage.js
+++ pixi/mainStage.js
@@ -1,4 +1,4 @@
-const { Sprite } = require('pixi.js');
+const { Sprite, TilingSprite } = require('pixi.js');
 const PIXI = require('pixi.js');
 const Project = require('../client/MainView');
 
@@ -7,7 +7,7 @@ const app = new PIXI.Application({
 	resizeTo: window,
 });
 
-app.renderer.backgroundColor = 0xb694e4;
+app.renderer.backgroundColor = 0x090135;
 let pixiDiv = document.getElementById('pixi');
 pixiDiv.appendChild(app.view);
 
@@ -79,6 +79,90 @@ function keyboard(value) {
 }
 
 //place to put background colors/textures
+
+//function to create homePage sprites
+function createHomeSprite(x, y, texture, type) {
+	const sprite = new Sprite(texture);
+	app.stage.addChild(sprite);
+	sprite.anchor.set(0.5);
+	sprite.position.x = x;
+	sprite.position.y = y;
+	//sprite.scale.set()
+	return sprite;
+}
+
+const wallPaper = PIXI.Texture.from(
+	'/siteAssets/wallpaper/BackgroundTexture.png'
+);
+const folder = PIXI.Texture.from('/siteAssets/welcome/PinkFolder.png');
+const welcomeSign = PIXI.Texture.from('/siteAssets/welcome/welcomeSign.png');
+const github = PIXI.Texture.from('/siteAssets/welcome/GithubBW.png');
+const linkedIn = PIXI.Texture.from('/siteAssets/welcome/LinkedInBW.png');
+const spotify = PIXI.Texture.from('/siteAssets/welcome/SpotifyBW.png');
+const gmail = PIXI.Texture.from('/siteAssets/welcome/GmailBW.png');
+
+const style = {
+	fontFamily: 'Nunito Sans',
+	fontSize: 25,
+	fontWeight: 'bold',
+};
+
+// let wallPaperSprite = createHomeSprite(
+// 	appWidth / 2,
+// 	appHeight / 2 + appHeight / 20,
+// 	wallPaper
+// );
+// wallPaperSprite.width = appWidth;
+// wallPaperSprite.height = appHeight - appHeight / 20;
+
+/* create a tiling sprite ...
+ * requires a texture, a width and a height
+ * in WebGL the image size should preferably be a power of two
+ */
+var tilingSprite = new TilingSprite(
+	wallPaper,
+	app.renderer.width,
+	app.renderer.height
+);
+app.stage.addChild(tilingSprite);
+//folder 1
+let folderSpriteOne = createHomeSprite(appWidth / 4, appHeight / 3.5, folder);
+folderSpriteOne.scale.set(0.25);
+let folderOneText = new PIXI.Text('About', style);
+folderOneText.visible = true;
+folderOneText.position.x =
+	folderSpriteOne.position.x - folderSpriteOne.position.x / 10;
+folderOneText.position.y =
+	folderSpriteOne.position.y - folderSpriteOne.position.y / 15;
+app.stage.addChild(folderOneText);
+//folder 2
+let folderSpriteTwo = createHomeSprite(
+	appWidth / 4,
+	appHeight / 3.5 + appHeight / 4,
+	folder
+);
+folderSpriteTwo.scale.set(0.25);
+let folderTwoText = new PIXI.Text('Projects', style);
+folderTwoText.visible = true;
+folderTwoText.position.x =
+	folderSpriteOne.position.x - folderSpriteTwo.position.x / 8;
+folderTwoText.position.y =
+	folderSpriteTwo.position.y - folderSpriteTwo.position.y / 30;
+app.stage.addChild(folderTwoText);
+//folder 3
+let folderSpriteThree = createHomeSprite(
+	appWidth / 4,
+	appHeight / 3.5 + (appHeight / 4) * 2,
+	folder
+);
+folderSpriteThree.scale.set(0.25);
+let folderThreeText = new PIXI.Text('Resume', style);
+folderThreeText.visible = true;
+folderThreeText.position.x =
+	folderSpriteThree.position.x - folderSpriteThree.position.x / 8;
+folderThreeText.position.y =
+	folderSpriteThree.position.y - folderSpriteThree.position.y / 40;
+app.stage.addChild(folderThreeText);
 
 //define scale
 
