8aa04642c8c45b76494e73f114bbe2a09dce5f45
Marie Crumpton <marie.k.cr@gmail.com>
2021-01-29 11:31:42 -0500

    adjusted mobile view

--- client/HeadShot.js
+++ client/HeadShot.js
@@ -4,9 +4,13 @@ import * as PIXI from 'pixi.js';
 
 export default class HeadShot extends React.Component {
   createPopUpRect(x, y, width, height) {
+    let closeSize = { x: x + 20, y: y + 20, radius: 15 };
     if (window.innerWidth < 1050) {
       x *= 0.5;
       width *= 1.5;
+      closeSize.x = (width / 2) * 0.3;
+      closeSize.y = (height / 4) * 0.17;
+      closeSize.radius = width * 0.03;
     }
     const rect = new PIXI.Graphics();
     rect.beginFill(0xe3cdfe).drawRect(x, y, width, height).endFill();
@@ -15,13 +19,20 @@ export default class HeadShot extends React.Component {
     const blur = new PIXI.filters.BlurFilter(3, 4);
     rect.filters = [blur];
     const bar = new PIXI.Graphics();
-    bar.beginFill(0x361876).drawRect(x, y, width, 30).endFill();
+    bar
+      .beginFill(0x361876)
+      .drawRect(x, y, width, width * 0.06)
+      .endFill();
     bar.visible = false; //set to false when I have click functionality
     PixiApp.headShotContainer.addChild(bar);
     const close = new PIXI.Graphics();
     close
       .beginFill(0xe5699d)
-      .drawCircle(x + 20, y + 15, 10)
+      .drawCircle(
+        closeSize.x,
+        closeSize.y,
+        closeSize.radius
+      ) /* (bar.width / 6) * 0.9, bar.height * 1.3, width * 0.03 */
       .endFill();
     close.visible = false; //set to false when I have click functionality
     close.interactive = true;
@@ -48,14 +59,23 @@ export default class HeadShot extends React.Component {
       messageX: (headshotPopUp.width / 2) * 1.05,
       messageY: (headshotPopUp.height / 4) * 0.33,
       photoScale: 0.22,
+      photoScaley: 0.22,
       photoX: (headshotPopUp.width / 2) * 1.55,
       photoY: (headshotPopUp.height / 2) * 1.17,
     };
     if (window.outerWidth < 500) {
-      stylePosition.style.fontSize = 80;
       stylePosition.messageX = (headshotPopUp.width / 2) * 0.4;
       stylePosition.photoScale = 0.32;
       stylePosition.photoX = headshotPopUp.width / 1.65;
+      if (window.outerHeight < 800) {
+        stylePosition.messageX = (headshotPopUp.width / 2) * 0.4;
+        stylePosition.style.fontSize = 80;
+        stylePosition.photoScaley = 0.36;
+      } else {
+        stylePosition.messageX = (headshotPopUp.width / 2) * 0.3;
+        stylePosition.style.fontSize = 85;
+        stylePosition.photoScaley = 0.45;
+      }
     } else if (window.outerWidth < 600) {
       stylePosition.style.fontSize = 60;
       stylePosition.messageX = (window.outerWidth / 2) * 0.85;
@@ -85,7 +105,7 @@ export default class HeadShot extends React.Component {
     const photo = new PIXI.Sprite(headshotTexture);
     photo.visible = false;
     photo.anchor.set(0.5);
-    photo.scale.set(stylePosition.photoScale);
+    photo.scale.set(stylePosition.photoScale, stylePosition.photoScaley);
     photo.position.x = stylePosition.photoX;
     photo.position.y = stylePosition.photoY;
     PixiApp.headShotContainer.addChild(photo);
